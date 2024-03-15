84cae2816d7bad1445c8e7387bc0bb506ec52cd0
taohidkhan22 <taohid130@gmail.com>
2024-03-15 10:12:52 +0600

    index.jsx modification

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx';
-import './index.css';
-//  import "@font-source/outfit";
-//  import "font-source/roboto";
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-/*ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename={"/Portfolio_React/"}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
- */ 
